9078cefbf0aa11618169be422f0c3cc4b692aa6e
Do Duy Dong <duydong.do.sdet@gmail.com>
2025-02-27 12:43:38 +0700

    Cross Browser Testing

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,8 +1,35 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
 import java.util.Random;
 
 public class BaseTest {
+    private WebDriver driver;
+
+    protected WebDriver getWebDriver(String browserName, String appUrl) {
+        switch (browserName) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Unsupported browser: " + browserName);
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(GlobalConstants.LONG_TIMEOUT));
+        driver.manage().window().maximize();
+        driver.get(appUrl);
+        return driver;
+    }
 
     protected int getRandomNumber() {
         return new Random().nextInt(10000);
