b722349d2ff7e6b87beba72992bfa6402c4576b2
Do Duy Dong <duydong.do.sdet@gmail.com>
2025-03-10 21:55:10 +0700

    Apply ExtentReports

--- actions/commons/GlobalConstants.java
+++ actions/commons/GlobalConstants.java
@@ -8,6 +8,10 @@ public class GlobalConstants {
     public static final long ONE_SECOND = 1;
 
     public static final String PROJECT_PATH = System.getProperty("user.dir");
-    public static final String UPLOAD_FILES_FOLDER = PROJECT_PATH + File.separator + "uploadFiles" + File.separator;
+    public static final String FILE_SEP = File.separator;
+    public static final String UPLOAD_FILES_FOLDER = PROJECT_PATH + FILE_SEP + "uploadFiles" + FILE_SEP;
+    public static final String EXTENTREPORTS_OUTPUT = PROJECT_PATH + FILE_SEP + "extentreports-output" + FILE_SEP + "extentreports.html";
+
+    public static final String OS_NAME = System.getProperty("os.name");
 
 }
