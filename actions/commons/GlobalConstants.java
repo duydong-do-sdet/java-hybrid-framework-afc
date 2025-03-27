552c4e63dd7a0f4ad457ab50b0145cdcb5f05c8f
Do Duy Dong <duydong.do.sdet@gmail.com>
2025-03-27 11:37:15 +0700

    Manage Test Data/ JSON file

--- actions/commons/GlobalConstants.java
+++ actions/commons/GlobalConstants.java
@@ -15,6 +15,7 @@ public class GlobalConstants {
     public static final String UPLOAD_FILES_FOLDER = PROJECT_BASE_PATH + "uploadFiles" + FILE_SEP;
     public static final String EXTENTREPORTS_OUTPUT = PROJECT_BASE_PATH + "extentreports-output" + FILE_SEP + "extentreports.html";
     public static final String ALLURE_REPORT_OUTPUT = PROJECT_BASE_PATH + "allure-results" + FILE_SEP;
+    public static final String DATA_JSON_PATH = PROJECT_BASE_PATH + "testdata" + FILE_SEP;
 
     public static final String OS_NAME = System.getProperty("os.name");
     public static final String JAVA_VERSION = System.getProperty("java.version");
